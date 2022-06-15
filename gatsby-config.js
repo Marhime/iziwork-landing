b07662a6e47022fda805dea10bb146e066f15f24
Marhime <maxime.joyes@gmail.com>
2022-06-15 00:35:12 -0600

    ga

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,26 +4,52 @@ module.exports = {
     siteUrl: `https://www.yourdomain.tld`
   },
   plugins: [
-    "gatsby-plugin-sass", {
-    resolve: 'gatsby-plugin-google-analytics',
-    options: {
-      "trackingId": "G-K9CY0DTZK5"
-    },
-    resolve: `gatsby-plugin-manifest`,
+    {
+      resolve: `gatsby-plugin-google-gtag`,
       options: {
-        name: "Onboarding Iziwork Italia",
-        short_name: "Onboarding Iziwork",
-        start_url: "/",
-        background_color: "#4040B2",
-        theme_color: "#4040B2",
-        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
-        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
-        display: "standalone",
-        icon: "static/new-logo-icon.svg", // This path is relative to the root of the site.
-        // An optional attribute which provides support for CORS check.
-        // If you do not provide a crossOrigin option, it will skip CORS for manifest.
-        // Any invalid keyword or empty string defaults to `anonymous`
-        crossOrigin: `use-credentials`,
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          "G-K9CY0DTZK5", // Google Analytics / GA
+        ],
+        // This object gets passed directly to the gtag config command
+        // This config will be shared across all trackingIds
+        gtagConfig: {
+          optimize_id: "OPT_CONTAINER_ID",
+          anonymize_ip: true,
+          cookie_expires: 0,
+        },
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: true,
+          // Setting this parameter is also optional
+          respectDNT: true,
+          // Avoids sending pageview hits from custom paths
+          exclude: ["/preview/**", "/do-not-track/me/too/"],
+          // Defaults to https://www.googletagmanager.com
+          origin: "YOUR_SELF_HOSTED_ORIGIN",
+        },
       },
-  }]
+    },
+    "gatsby-plugin-sass",
+    {
+
+      resolve: `gatsby-plugin-manifest`,
+        options: {
+          name: "Onboarding Iziwork Italia",
+          short_name: "Onboarding Iziwork",
+          start_url: "/",
+          background_color: "#4040B2",
+          theme_color: "#4040B2",
+          // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+          // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+          display: "standalone",
+          icon: "static/new-logo-icon.svg", // This path is relative to the root of the site.
+          // An optional attribute which provides support for CORS check.
+          // If you do not provide a crossOrigin option, it will skip CORS for manifest.
+          // Any invalid keyword or empty string defaults to `anonymous`
+          crossOrigin: `use-credentials`,
+        },
+    }
+  ]
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -119,22 +119,10 @@ const IndexPage = ({location, data}) => {
                   <div className="pdf-section">
                     <div className="pdf-section__item">
                       <a target="_blank" 
-                      // href='/La-struttura-del-contratto.pdf' 
+                      href='/La-struttura-del-contratto.pdf' 
                       className="pdf-section__item-button"
                       onClick={e => {
-                        // To stop the page reloading
-                        e.preventDefault()
-                        // Lets track that custom click
-                        trackCustomEvent({
-                          // string - required - The object that was interacted with (e.g.video)
-                          category: "Special Button",
-                          // string - required - Type of interaction (e.g. 'play')
-                          action: "Click",
-                          // string - optional - Useful for categorizing events (e.g. 'Spring Campaign')
-                          label: "Gatsby Plugin Example Campaign",
-                          // number - optional - Numeric value associated with the event. (e.g. A product ID)
-                          value: 43,
-                        })
+                        window.gtag("event", "click", 'La struttura del contratto pdf')
                       }}
                       >La struttura del contratto <svg width="15" height="18" viewBox="0 0 15 18" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M8.4137 1.5C8.4137 0.947715 7.96598 0.5 7.4137 0.5C6.86141 0.5 6.4137 0.947715 6.4137 1.5L8.4137 1.5ZM6.70659 17.2071C7.09711 17.5976 7.73028 17.5976 8.1208 17.2071L14.4848 10.8431C14.8753 10.4526 14.8753 9.81946 14.4848 9.42893C14.0942 9.03841 13.4611 9.03841 13.0706 9.42893L7.4137 15.0858L1.75684 9.42893C1.36632 9.03841 0.733152 9.03841 0.342628 9.42893C-0.0478962 9.81946 -0.0478962 10.4526 0.342628 10.8431L6.70659 17.2071ZM6.4137 1.5L6.4137 16.5L8.4137 16.5L8.4137 1.5L6.4137 1.5Z" fill="white"/></svg></a><p className="pdf-section__item-size">2 Mo</p></div>
                   </div>
